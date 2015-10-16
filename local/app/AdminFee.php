b19c6177168984a96cfc63e6178cdafead4ddf20
Maria Monica I. Jimenez <mamonica.jimenez@gmail.com>
2015-10-16 12:06:09 +0800

    Version 1 - Admin Fee Completed

--- local/app/AdminFee.php
+++ local/app/AdminFee.php
@@ -6,34 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 // additional includes
 use Datetime;
-use DB;
 
 class AdminFee extends Model
 {
-    protected $table = 'adminfee';
+    protected $table = 'dbo.adminfee';
     protected $primaryKey = 'admin_code';
     public $timestamps = false;
 
     public function addAdminFee($admin_flag= '', $amount = '',$remarks = '',$request_id = '',$user_id = '')
     {
-        try {
-            $this->admin_remarks = $remarks;
-            $this->admin_fee = $amount;
-            $this->admin_date = new DateTime('today');
-            $this->admin_flag = $admin_flag;
-            $this->app_code = $user_id;
-            $this->rfc_code = $request_id;
+    	$this->admin_remarks = $remarks;
+    	$this->admin_fee = $amount;
+    	$this->admin_date = new DateTime('today');
+    	$this->admin_flag = $admin_flag;
+    	$this->app_code = $user_id;
+    	$this->rfc_code = $request_id;
 
-            $this->save();
+    	$this->save();
 
-            if($this->admin_code)
-            {
-                return true;
-            }
-
-            return false;
-        } catch (\Exception $e) {
-            return false;
-        }
+    	return true;
     }
 }
